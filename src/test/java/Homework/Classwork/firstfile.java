ef921a289c8191be95978eae008b5a8daa4751c1
param <param759@gmail.com>
2021-09-21 22:13:50 -0400

    very first file

---
+++ src/test/java/Homework/Classwork/firstfile.java
@@ -0,0 +1,18 @@
+package Homework.Classwork;
+
+import org.testng.annotations.Test;
+
+public class firstfile {
+
+	@Test
+	public void title()
+	{
+		System.out.println("first file of git");
+	}
+	
+	@Test
+	public void title2()
+	{
+		System.out.println("Second file of git");
+	}
+}
---
+++ src/test/java/Homework/Classwork/secondfile.java
@@ -0,0 +1,19 @@
+package Homework.Classwork;
+
+import org.testng.annotations.Test;
+
+public class secondfile {
+	
+	@Test
+	
+	public void rest()
+	{
+		System.out.println("file completion");
+	}
+
+	@Test
+	public void rest2()
+	{
+		System.out.println("file not completion");
+	}
+}
